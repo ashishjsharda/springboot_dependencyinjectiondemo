3a3ae41896ce3afa39d2c32e07a1ffe17675f0a8
Ashish Sharda <ashishjsharda@gmail.com>
2023-10-01 07:12:02 -0400

    first commit

---
+++ src/main/java/com/example/dependencyinjectiondemo/DependencyinjectiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectiondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyinjectiondemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dependencyinjectiondemo/DependencyinjectiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyinjectiondemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
